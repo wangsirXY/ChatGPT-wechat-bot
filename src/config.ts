310b929b86ae9e8fabbbd6a52a0d91b3250c1cd1
GuAn <15713912912@163.com>
2023-03-30 10:14:36 +0800

    init: :tada: initial commit

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+/**
+  * @description 配置文件
+  * @author GuAn
+  * @time 2023-03-30 10:12:29
+  */
+export default {
+  // 云函数请求地址
+  API_BASE_URL: "",
+  // 填入你的OPENAI_API_KEY
+  OPENAI_API_KEY: '',
+  // 设置获取消息的重试次数
+  retryTimes: 3,
+  // 在群组中设置唤醒微信机器人的关键词
+  groupKey: '',
+  // 在私聊中设置唤醒微信机器人的关键词
+  privateKey: '',
+  // 重置上下文的关键词，如可设置为reset
+  resetKey: 'reset',
+  // 开启会后收到ChatGPT的自动回复
+  autoReply: true,
+  // 根据正则匹配是否自动通过好友验证
+  friendShipRule: /chatgpt|chat/,
+  // 是否在群聊中按照回复的格式进行回复
+  groupReplyMode: true,
+  // 是否在私聊中按照回复的格式进行回复
+  privateReplyMode: false,
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/**
+  * @description 入口文件
+  * @author GuAn
+  * @time 2023-03-30 10:13:06
+  */
