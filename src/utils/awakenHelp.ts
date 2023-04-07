5e1bc409987ad6abe46522d09f1a3206581de9c4
GuAn <15713912912@163.com>
2023-04-07 11:16:27 +0800

    fix: :bug: 修复私聊“help指令”提示词异常BUG

--- src/utils/awakenHelp.ts
+++ src/utils/awakenHelp.ts
@@ -5,11 +5,11 @@
   */
 export default function (nickname?: string) {
   return `
-    nickname${nickname ? `@${nickname} ` : ''}我会为您提供以下帮助：\n
+    ${nickname ? `@${nickname} ` : ''}我会为您提供以下帮助：\n
     \n
-    1. 发送私聊信息“小安 + 问题”，即可唤醒我为您解答.\n
-    2. 发送好友验证信息“chatgpt | chat”，即可添加我为好友.\n
+    1.发送私聊信息“小安 + 问题”，即可唤醒我为您解答.\n
+    2.发送好友验证信息“chatgpt | chat”，即可添加我为好友.\n
     \n
     当然，这些只是我已经掌握的指令，今后我会为您提供更多的帮助.
-  `
+  `.trim();
 };
